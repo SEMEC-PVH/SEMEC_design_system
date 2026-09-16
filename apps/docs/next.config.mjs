5eb0597d73a27ffd54fda257e3c5eef24fc0723b
rug-gieri <rafaelruggieri.ti@gmail.com>
2026-09-16 15:27:16 -0400

    fix(docs): adiciona basePath para GitHub Pages

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -3,6 +3,7 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/SEMEC_design_system',
   trailingSlash: true,
   allowedDevOrigins: ['10.102.3.109'],
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
